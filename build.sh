0d77a24d7afa6fd40a219ce544446a5aa29321bd
jcancelli <jonathan.cancelli.46@gmail.com>
2023-11-10 12:59:12 +0100

    Remove ROOT_INCLUDE variable from build script

--- build.sh
+++ build.sh
@@ -2,7 +2,6 @@
 OUT_DIR=out
 
 COMPILER_ARGS="$(root-config --cflags --libs) -Wall -Wextra"
-ROOT_INCLUDE=$ROOTSYS/include
 
 SRC_FILES="\
 	src/particle_type.cpp \
@@ -17,11 +16,11 @@ TARGET_BIN=$OUT_DIR/main
 
 if [ "$1" == "run" ] || [ $# -eq 0 ]
 then
-	g++ -I$ROOT_INCLUDE -o $TARGET_BIN $SRC_FILES $MAIN $COMPILER_ARGS
+	g++ -o $TARGET_BIN $SRC_FILES $MAIN $COMPILER_ARGS
 	./${TARGET_BIN}
 elif [ "$1" == "test" ]
 then
-	g++ -I$ROOT_INCLUDE -o $TEST_BIN $SRC_FILES $TEST $COMPILER_ARGS
+	g++ -o $TEST_BIN $SRC_FILES $TEST $COMPILER_ARGS
 	./${TEST_BIN}
 else
 	echo "Synthax: ./build.sh <run|test>"
