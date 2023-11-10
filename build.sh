e01dd5cfae65fa18e81b626326eec6c15e9e2abf
jcancelli <jonathan.cancelli.46@gmail.com>
2023-11-10 15:29:02 +0100

    Add analisis and build-analisis commands to build script

--- build.sh
+++ build.sh
@@ -8,13 +8,19 @@ SRC_FILES="\
 	src/resonance_type.cpp \
 	src/particle.cpp"
 SIMULATION=src/simulation.cpp
+ANALISIS=src/analisis.cpp
 TEST=src/test.cpp
 
 TEST_BIN=$OUT_DIR/test
 SIMULATION_BIN=$OUT_DIR/simulation
+ANALISIS_BIN=$OUT_DIR/analisis
 
 function build-simulation() {
 	g++ -o $SIMULATION_BIN $SRC_FILES $SIMULATION $COMPILER_ARGS
+}
+
+function build-analisis() {
+	g++ -o $ANALISIS_BIN $SRC_FILES $ANALISIS $COMPILER_ARGS
 }
 
 function build-test() {
@@ -22,15 +28,24 @@ function build-test() {
 }
 
 function help() {
-	echo "Synthax: ./build.sh <simulation|test|build-simulation|build-test>"
+	echo "Synthax: ./build.sh <analisis|simulation|test|build-analisis|build-simulation|build-test>"
 	echo ""
+	echo analisis - Build and run analisis
+	echo build-analisis - Build analisis
 	echo simulation - Build and run simulation
 	echo build-simulation - Build main program
 	echo test - Build and run tests
 	echo build-test - Build tests
 }
 
-if [ "$1" == "simulation" ] || [ $# -eq 0 ]
+if [ "$1" == "analisis" ] || [ $# -eq 0 ]
+then
+	$(build-analisis) \
+	&& ./${ANALISIS_BIN}
+elif [ "$1" == "build-analisis" ]
+then
+	$(build-analisis)
+elif [ "$1" == "simulation" ]
 then
 	$(build-simulation) \
 	&& ./${SIMULATION_BIN}
