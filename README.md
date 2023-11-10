73b1490478ed736be264f5812fa31448dbdd676b
jcancelli <jonathan.cancelli.46@gmail.com>
2023-11-10 13:15:21 +0100

    Add build and build-test commands to build script

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 
 ## Build commands
 
-| Command            | Description                  |
-|--------------------|------------------------------|
-|`./build.sh test`   | Compile and run tests        |
-|`./build.sh run`    | Compile and run main program |
+| Command               | Description                  |
+|-----------------------|------------------------------|
+|`./build.sh test`      | Compile and run tests        |
+|`./build.sh run`       | Compile and run main program |
+|`./build.sh build-test`| Compile tests                |
+|`./build.sh build`     | Compile main program         |
--- build.sh
+++ build.sh
@@ -16,15 +16,23 @@ TARGET_BIN=$OUT_DIR/main
 
 if [ "$1" == "run" ] || [ $# -eq 0 ]
 then
+	g++ -o $TARGET_BIN $SRC_FILES $MAIN $COMPILER_ARGS \
+	&& ./${TARGET_BIN}
+elif [ "$1" == "build" ]
+then
 	g++ -o $TARGET_BIN $SRC_FILES $MAIN $COMPILER_ARGS
-	./${TARGET_BIN}
 elif [ "$1" == "test" ]
 then
+	g++ -o $TEST_BIN $SRC_FILES $TEST $COMPILER_ARGS \
+	&& ./${TEST_BIN}
+elif [ "$1" == "build-test" ]
+then
 	g++ -o $TEST_BIN $SRC_FILES $TEST $COMPILER_ARGS
-	./${TEST_BIN}
 else
 	echo "Synthax: ./build.sh <run|test>"
 	echo ""
 	echo run - Build and run main program
+	echo build - Build main program
 	echo test - Build and run tests
+	echo build-test - Build tests
 fi
