4a8ecf508b829a1d642d2adf6204e566212cdf1d
jcancelli <jonathan.cancelli.46@gmail.com>
2023-12-15 17:21:51 +0100

    Implement concat function

--- src/util.hpp
+++ src/util.hpp
@@ -1,9 +1,23 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+#include <string>
 
 void section(const char* title);
 
 const char* boolToString(bool b);
 
 bool equals(double a, double b, double epsilon);
+
+template <class T>
+std::string concat(const T& arg) {
+  std::stringstream ss;
+  ss << arg;
+  return ss.str();
+}
+
+template <class T, class... Args>
+std::string concat(const T& first, const Args&... args) {
+  return concat(first) + concat(args...);
+}
